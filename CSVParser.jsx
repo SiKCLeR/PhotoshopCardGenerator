ae65444c7c28fe926db750b24baebe02630de193
Simon Chocquet-Le Roy <simon.chocquetbottani@gmail.com>
2020-01-31 15:20:01 +0100

    working parser!

--- CSVParser.jsx
+++ CSVParser.jsx
@@ -1,4 +1,78 @@
-function testCSVParser()
+function CSVData(_filePath) // Class definition (yup it's weird in JSX)
 {
-    alert('test');
-}
+    // Class variables
+    this.m_entryTypes = null;
+    this.m_entryList = [];
+
+    var fs = new File(_filePath);
+    fs.open('r');
+    var curEntry = '';
+    var curChar = '';
+    var curEntryList = [];
+    curChar += fs.readch();
+
+    while(!fs.eof)
+    {
+        if(curChar == ',') // Next Entry
+        {
+            curEntryList.push(curEntry);
+            curEntry = '';
+        }
+        else if(curChar == '\n') // Next Line
+        {
+            curEntryList.push(curEntry);
+            if(this.m_entryTypes == null)
+            {
+                this.m_entryTypes = curEntryList;
+            }
+            else 
+            {
+                alert(curEntryList.length)
+                //this.m_entryList.push([]);
+                //this.m_entryList[this.m_entryList.length-1] = curEntryList;
+                this.m_entryList[this.m_entryList.length] = curEntryList;
+            }
+            curEntry = '';
+            curEntryList = [];
+        }
+        else // Else keep feeling the current entry
+        {
+            curEntry += curChar;
+        }
+        curChar = fs.readch();
+    }
+
+    if(curEntry != "")
+    {
+        curEntryList.push(curEntry);
+        this.m_entryList[this.m_entryList.length] = curEntryList;
+        //this.m_entryList.push([]);
+        //this.m_entryList[this.m_entryList.length-1].push(curEntryList);
+    }
+
+    //alert(curEntry)
+    fs.close();
+    //alert(this.m_entryTypes)
+    //alert(this.m_entryList.length);
+    //alert(this.m_entryList[0].length);
+};
+
+CSVData.prototype.printCSVData = function()
+{
+    //alert(this.m_entryTypes)
+    prtInfos = 'Entries Titles:\n';
+    for(var et in this.m_entryTypes)
+    {
+        prtInfos += this.m_entryTypes[et] + '|';
+    }
+    prtInfos += '\n';
+    for(var el in this.m_entryList)
+    {
+        for(var e in this.m_entryList[el])
+        {
+            prtInfos += this.m_entryList[el][e] + '|';
+        }
+        prtInfos += '\n';
+    }
+    alert(prtInfos);
+};
--- helloWorld.jsx
+++ helloWorld.jsx
@@ -1,75 +1,18 @@
 //alert('hello world');
-//#include "CSVParser.jsx"
-
-function CSVData(_filePath)
-{
-    //this.filepath = _filePath;
-    //this.entryTypes = null;
-    this.entryList = [];
-
-    var fs = new File(_filePath);
-    fs.open('r');
-    var curEntry = '';
-    var curChar = '';
-    var curEntryList = [];
-    curChar += fs.readln();
-    while(!fs.eof)
-    {
-        if(curChar == ',')
-        {
-            curEntryList.push(curEntry);
-            curEntry = '';
-        }
-        else if(curChar == '\n')
-        {
-            curEntryList.push(curEntry);
-            if(this.entryTypes == null)
-            {
-                this.entryTypes = curEntryList;
-            }
-            else 
-            {
-                this.entryList.length = this.entryList.length+1;
-                this.entryList[this.entryList.length-1] = [];
-                this.entryList[this.entryList.length-1].push(curEntry);
-            }
-            curEntry = "";
-            curEntryList = [];
-        }
-        else 
-        {
-            
-        }
-        curChar += fs.readln();
-    }
-    fs.close()
-}
-
-CSVData.prototype.printCSVData = function()
-{
-    prtInfos = 'Entries Titles\n';
-    for(et in this.entryTypes)
-    {
-        prtInfos += et + '|';
-    }
-    prtInfos += '\n';
-    for(el in this.entryList)
-    {
-        for(e in el)
-        {
-            prtInfos += e + '|';
-        }
-        prtInfos += '\n';
-    }
-    alert(prtInfos);
-}
+//include "CSVParser.jsx"
+//import "CSVParser.jsx"
+#include "D:/Projets/Les_Mondes_D_Olim/PhotoshopScript/CSVParser.jsx"
 
 (function main()
 {
     //testCSVParser();
     //alert('hello world');
-    var testCSVData = new CSVData("D:/Projets/Les_Mondes_D_Olim/PhotoshopScript/Ressources/Les Mondes d'Olim - Creatures.csv")
-    testCSVData.printCSVData()
+    //alert('hello world');
+    var testCSVData = new CSVData("D:/Projets/Les_Mondes_D_Olim/PhotoshopScript/Ressources/Les Mondes d'Olim - Creatures.csv");
+    //testCSVParser()
+    testCSVData.printCSVData();
+    alert(testCSVData.m_entryTypes);
+    alert(testCSVData.m_entryList);
 })();
 
 
