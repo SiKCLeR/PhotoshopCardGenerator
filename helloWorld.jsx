2078789e01c1899a1c8124687b5ac26f3f2a6ec0
Simon Chocquet-Le Roy <simon.chocquetbottani@gmail.com>
2020-07-27 08:00:19 +0200

    firt version of card generation

--- helloWorld.jsx
+++ helloWorld.jsx
@@ -4,70 +4,132 @@
 #include "D:/Projets/Les_Mondes_D_Olim/PhotoshopScript/CSVParser.jsx"
 //#include "CSVParser.jsx"
 //#include "../CSVParser.jsx"
-function GenerateCards(filePath)
+
+function ParseLayerPath(path)
 {
-    var testCSVData = new CSVData("D:/Projets/Les_Mondes_D_Olim/PhotoshopScript/Ressources/Les Mondes d'Olim - Cards Sheet.csv");
-    var entriesAct = [];
-    var entriesPath = [];
-    alert(testCSVData.m_entryTypes)
-    for(t in testCSVData.m_entryTypes)   
-    {
-        var rawTypeData = t.split('_');
-        entriesAct.push(rawTypeData[0]);
-        var curEntryPath = [];
-        
-        for(var i = 1; i < rawTypeData.length; i++)
-        {
-            curEntryPath.push(rawTypeData[i]);
-        }
-        entriesPath.push(curEntryPath);
-    }
+    return path.split('/');
+}
 
-    //for(a in entriesAct)
-    //{
-        alert(entriesAct);
-    //}
+function GetLayer(curLayer, layerPathAr)
+{
+    var layerName = layerPathAr.shift();
+    //var layer = curLayer.GetByName(layerName);
 
-    for (e in testCSVData.m_entryList) 
+    for(var x = 0; x < curLayer.layers.length; x++)
     {
-        for(a in entriesAct)
+        if(curLayer.layers[x].name == layerName)
         {
-            switch(a)
+            if(layerPathAr.length <= 0)
             {
-                case "ID":
-                    alert("ID");
-                    break;
-                case "TEMPLATE":
-                    alert("TEMPLATE");
-                    break;
-                case "IMG":
-                    alert("IMG");
-                    break;
-                case "LAYER":
-                    alert("LAYER");
-                    break;
-                case "TEXT":
-                    alert("TEXT");
-                    break;
-                default:
-                    alert("Unknown action type")
-                    break;
+                return curLayer.layers[x];
+            }
+            else
+            {
+                return GetLayer(curLayer.layers[x], layerPathAr)
             }
         }
     }
-    alert(testCSVData.m_entryTypes[0].split('_'));
+}
+
+function GetLayerFromRoot(layerPathAr)
+{
+    return GetLayer(app.activeDocument, layerPathAr);
+}
+
+function SetTextLayer(layerPath, text)
+{
+    var layer = GetLayerFromRoot(ParseLayerPath(layerPath));
+    layer.textItem.contents = text;
+}
+
+function SetOnlyActiveLayer(layerParentPath, onlyActiveName)
+{
+    var parentLayer = GetLayerFromRoot(ParseLayerPath(layerParentPath));
+    for(var x = 0; x < parentLayer.layers.length; x++)
+    {
+        if(parentLayer.layers[x].name == onlyActiveName)
+        {
+            parentLayer.layers[x].visible = true;
+        }
+        else
+        {
+            parentLayer.layers[x].visible = false;
+        }
+    }
+}
+
+function GenerateCards(filePath)
+{
+    var parsedCSV = new CSVData(filePath);
+    var entriesAct = [];
+    var entriesPath = [];
+    var cardID = "";
+
+    for (var u = 0; u < parsedCSV.m_entryList.length; u++ ) 
+    {
+        for(var i = 0; i < parsedCSV.m_entryTypes.length; i++)
+        {
+            var entryTypeAr = parsedCSV.m_entryTypes[i].split('@');
+            switch(entryTypeAr[0])
+            {
+                case "ID":
+                    entriesPath = parsedCSV.m_entryList[u][i];
+                    //alert("ID");
+                    break;
+                case "TEMPLATE":
+                    //alert("TEMPLATE");
+                    break;
+                case "IMG":
+                    //alert("IMG");
+                    break;
+                case "LAYER":
+                    SetOnlyActiveLayer(entryTypeAr[1], parsedCSV.m_entryList[u][i]);
+                    //alert("Layer Action Path: " + entryTypeAr[1] + "\nProp:" + parsedCSV.m_entryList[u][i]);
+                    break;
+                case "TEXT":
+                    SetTextLayer(entryTypeAr[1], parsedCSV.m_entryList[u][i]);
+                    //alert("Text Action Path: " + entryTypeAr[1] + "\nProp:" + parsedCSV.m_entryList[u][i]);
+                    break;
+                default:
+                    //alert("Unknown action type")
+                    break;
+            }
+        }
+        var saveFile = new File("D:/Projets/Les_Mondes_D_Olim/PhotoshopScript/Export/" + entriesPath + "_" + u +".png");
+        var pngOpts = new PNGSaveOptions();
+        pngOpts.interlaced = false;
+        activeDocument.saveAs(saveFile, pngOpts, true, Extension.LOWERCASE);
+        //app.activeDocument.saveAs(new File("D:/Projets/Les_Mondes_D_Olim/PhotoshopScript/Export/Exported/"+u+".png"));
+    }
+    //alert(parsedCSV.m_entryTypes[0].split('_'));
 }
 
 (function main()
 {
+    GenerateCards("D:/Projets/Les_Mondes_D_Olim/PhotoshopScript/Ressources/Les Mondes d'Olim - Cards Sheet.csv");
+    //alert(app.path.fsName)
+    //parsedCSV = new CSVData("D:/Projets/Les_Mondes_D_Olim/PhotoshopScript/Ressources/Les Mondes d'Olim - Cards Sheet.csv");
     //testCSVParser();
     //alert('hello world');
     //alert('hello world');
-    var testCSVData = new CSVData("D:/Projets/Les_Mondes_D_Olim/PhotoshopScript/Ressources/Les Mondes d'Olim - Cards Sheet.csv");
+    //var testCSVData = new CSVData("D:/Projets/Les_Mondes_D_Olim/PhotoshopScript/Ressources/Les Mondes d'Olim - Cards Sheet.csv");
     //testCSVParser()
     //testCSVData.printCSVData();
     //app.activeDocument.layers[0].textItem.contents = testCSVData.m_entryList[0][2]
-    alert(testCSVData.m_entryTypes[0].split('_'));
+    /*var str = "";
+    for(var x = 0; x < app.activeDocument.layers.length; x++)
+    {
+        str += app.activeDocument.layers[x].name + "\n";
+    }
+    alert(str);
+
+    
+    var l = app.activeDocument.GetByName("TITLE");
+    alert(l.testItem.contents);*/
+    // Function tests
+    //SetTextLayer("TEST01/TEST02/TEST", "Lolilol");
+    //SetOnlyActiveLayer("STYLE/AGILITY");
+    //alert(testCSVData.m_entryTypes[0].split('_'));
     //alert(testCSVData.m_entryTypes);
     //alert(testCSVData.m_entryList);
 })();
